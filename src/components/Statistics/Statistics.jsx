1d92812f919d41064ac3b342275d819f4034777d
Anna-Sosiura <132984503+Anna-Sosiura@users.noreply.github.com>
2023-11-14 23:37:18 +0200

    commit(5)

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,25 +1,26 @@
+import { List, Item } from './Statistics.styled';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className="statistics">
       <h2 className="title">{title}</h2>
-      <ul className="stat-list">
-        <li className="item" key={stats[0].id}>
+      <List className="stat-list">
+        <Item className="item" key={stats[0].id}>
           <span className="label">{stats[0].label}</span>
           <span className="percentage">{stats[0].percentage}%</span>
-        </li>
-        <li className="item" key={stats[1].id}>
+        </Item>
+        <Item className="item" key={stats[1].id}>
           <span className="label">{stats[1].label}</span>
           <span className="percentage">{stats[1].percentage}%</span>
-        </li>
-        <li className="item" key={stats[2].id}>
+        </Item>
+        <Item className="item" key={stats[2].id}>
           <span className="label">{stats[2].label}</span>
           <span className="percentage">{stats[2].percentage}%</span>
-        </li>
-        <li className="item" key={stats[3].id}>
+        </Item>
+        <Item className="item" key={stats[3].id}>
           <span className="label">{stats[3].label}</span>
           <span className="percentage">{stats[3].percentage}%</span>
-        </li>
-      </ul>
+        </Item>
+      </List>
     </section>
   );
 };
