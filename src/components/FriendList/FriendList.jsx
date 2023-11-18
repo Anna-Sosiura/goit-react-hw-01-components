0a315795578bd2c9d3ab252659e75faf22de2f76
Anna-Sosiura <132984503+Anna-Sosiura@users.noreply.github.com>
2023-11-18 18:52:04 +0200

    commit(2.1)

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,32 +1,44 @@
+// import friends from '.../data/friends.json.';
 import { FriendListItem } from '../FriendListItem/FriendListItem';
 import { List } from './FriendList.styled';
 export const FriendList = ({ friends }) => {
   return (
     <List className="friend-list">
-      <FriendListItem
-        avatar={friends[0].avatar}
-        name={friends[0].name}
-        isOnline={friends[0].isOnline}
-        id={friends[0].id}
-      />
-      <FriendListItem
-        avatar={friends[1].avatar}
-        name={friends[1].name}
-        isOnline={friends[1].isOnline}
-        id={friends[1].id}
-      />
-      <FriendListItem
-        avatar={friends[2].avatar}
-        name={friends[2].name}
-        isOnline={friends[2].isOnline}
-        id={friends[2].id}
-      />
-      <FriendListItem
-        avatar={friends[3].avatar}
-        name={friends[3].name}
-        isOnline={friends[3].isOnline}
-        id={friends[3].id}
-      />
+      {friends.map(friend => (
+        <FriendListItem
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+          id={friend.id}
+        />
+      ))}
     </List>
+
+    // <List className="friend-list">
+    //   <FriendListItem
+    //     avatar={friends[0].avatar}
+    //     name={friends[0].name}
+    //     isOnline={friends[0].isOnline}
+    //     id={friends[0].id}
+    //   />
+    //   <FriendListItem
+    //     avatar={friends[1].avatar}
+    //     name={friends[1].name}
+    //     isOnline={friends[1].isOnline}
+    //     id={friends[1].id}
+    //   />
+    //   <FriendListItem
+    //     avatar={friends[2].avatar}
+    //     name={friends[2].name}
+    //     isOnline={friends[2].isOnline}
+    //     id={friends[2].id}
+    //   />
+    //   <FriendListItem
+    //     avatar={friends[3].avatar}
+    //     name={friends[3].name}
+    //     isOnline={friends[3].isOnline}
+    //     id={friends[3].id}
+    //   />
+    // </List>
   );
 };
